5dd391f28d887716d610ab04d6ca330eaa0adc50
Kai Davenport <kaiyadavenport@gmail.com>
2013-06-25 14:57:45 +0000

    moved auth into base

--- src/index.js
+++ src/index.js
@@ -22,9 +22,10 @@ var EventEmitter = require('events').EventEmitter;
 var engines = require('consolidate');
 var http = require('http');
 var express = require('express');
+var partials = require('express-partials');
 var socketio = require('socket.io');
 var RedisStore = require('./redisstore')(express);
-var passportSocketIo = require('passport.socketio');
+var passport = require('passport');
 var url = require('url');
 var fs = require('fs');
 var send = require('send');
@@ -38,6 +39,7 @@ function factory(options){
     document_root:__dirname+'/www',
     view_root:__dirname+'/views',
     templates:null,
+    cookieKey:'sdf8sdfsd8f78sd78',
     cookieSecret:'sedf98s7dfjsdfi',
     redis:{
       host:'127.0.0.1',
@@ -47,54 +49,68 @@ function factory(options){
 
   var app = express();
   var server = http.createServer(app);
+  var preparestack = {
+    before:[],
+    normal:[],
+    after:[]
+  };
+
+  app.prepare = function(phase, fn){
+    if(!fn){
+      fn = phase;
+      phase = null;
+    }
+    phase = phase || 'normal';
+    if(!preparestack[phase]){
+      preparestack[phase] = [];
+    }
+    preparestack[phase].push(fn);
+  }
+
+  app.run_prepares = function(){
+    var groups = _.map([
+      'before',
+      'normal',
+      'after'
+    ], function(phase){
+      return preparestack[phase];
+    })
+
+    _.each(groups, function(arr){
+      _.each(arr, function(fn){
+        fn.apply(app, []);
+      })
+    })
+  }
 
 
   /*
-  
+
     CORE
     
   */
+  app.cookieMaxAge = 60*60;
+  app.sessionStore = new RedisStore(options.redis);
 
   app.use(express.favicon(options.favicon));
   app.use(express.query());
   app.use(express.responseTime());
+  app.use(express.cookieParser(options.cookieSecret));
   app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({
+    key: options.cookieKey,
+    store: app.sessionStore,
+    secret: options.cookieSecret,
+    cookie: { maxAge: (app.cookieMaxAge !== 0) ? app.cookieMaxAge : null }
+  }))
 
-  app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.use(function(req, res, next){
-
-      /*
-      
-        no-caching unless we are live
-        
-      */
-      res.on('header', function(){
-        res.setHeader('cache-control', 'no-cache');
-        res.setHeader('pragma', 'no-cache');
-        res.setHeader('expires', '-1');
-      })
-      next();
-    })
-  })
-
-  app.configure('production', function(){
-    app.use(express.errorHandler());
-  })
-
-
-  /*
-  
-    TEMPLATES
-    
-  */
-  if(options.templates){
-    app.engine('ejs', engines.ejs);
-    app.set('view engine', 'ejs');
-    app.set('views', options.view_root);
+  if(options.auth){
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.passport = passport;  
   }
-
-
+  
   /*
   
     SOCKETS
@@ -118,30 +134,13 @@ function factory(options){
       'jsonp-polling'
     ])
 
-  }
-
-  /*
-  
-    AUTH
-    
-  */
-  if(options.auth){
-
-    app.cookieParser = express.cookieParser(options.cookieSecret);
-    app.sessionStore = new RedisStore(options.redis);
-
-    app.use(app.cookieParser);
-    app.use(express.session({
-      store: app.sessionStore,
-      secret: options.cookieSecret
-    }))
-
+        /*
     if(options.sockets){
       app.io.set('authorization', passportSocketIo.authorize({
         key:'connect.sid',
         cookieParser:app.cookieParser,
-        secret:options.cookieSecret,
         store:app.sessionStore,
+        secret:options.cookieSecret,
         fail:function(data, accept){
           accept(null, true);
         },
@@ -150,34 +149,63 @@ function factory(options){
         }
       }))
     }
+    */
+
   }
 
-  app.prepare = function(fn){
-    this._prepare = fn;
-  }
+  /*
+  
+    NORMAL
+    
+  */
+  app.prepare('normal', function(){
+
+    /*
+    
+      ERROR & CACHING
+      
+    */
+    app.configure('development', function(){
+      app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+      app.use(function(req, res, next){
+
+        /*
+        
+          no-caching unless we are live
+          
+        */
+        res.on('header', function(){
+          res.setHeader('cache-control', 'no-cache');
+          res.setHeader('pragma', 'no-cache');
+          res.setHeader('expires', '-1');
+        })
+        next();
+      })
+    })
+
+    app.configure('production', function(){
+      app.use(express.errorHandler());
+    })
+
+
+    /*
+    
+      TEMPLATES
+      
+    */
+    if(options.templates){
+      app.engine('ejs', engines.ejs);
+      app.set('view engine', 'ejs');
+      app.set('views', options.view_root);
+      app.use(partials());
+    }
+
+  })
 
   app.start = function(done){
    
-    if(this._prepare){
-      this._prepare();
-    }
-    
+    app.run_prepares();
     app.use(app.router);
-
-    function error(err) {
-      res.statusCode = err.status || 500;
-      res.end(err.message);
-    }
-
-    function redirect() {
-      res.statusCode = 301;
-      res.setHeader('Location', req.url + '/');
-      res.end('Redirecting to ' + req.url + '/');
-    }
-
-    function emitstream() {
-      app.emit('stream', true);
-    }
 
     /*
     
@@ -187,6 +215,22 @@ function factory(options){
     if(fs.existsSync(options.document_root)){
 
       app.use(function(req, res, next){
+
+        function error(err) {
+          res.statusCode = err.status || 500;
+          res.end(err.message);
+        }
+
+        function redirect() {
+          res.statusCode = 301;
+          res.setHeader('Location', req.url + '/');
+          res.end('Redirecting to ' + req.url + '/');
+        }
+
+        function emitstream() {
+          app.emit('stream', true);
+        }
+
         send(req, url.parse(req.url).pathname)
           .root(options.document_root)
           .on('error', error)
